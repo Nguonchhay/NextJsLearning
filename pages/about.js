c0a0f13df12064a340150091bfde5f2cef4c2667
Nguonchhay <nguonchhay.touch@gmail.com>
2019-11-24 19:16:32 +0700

    [TASK] Kick start and prepare layout

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import DefaultLayout from './../components/DefaultLayout';
+
+const About = () => (
+    <DefaultLayout>
+      <p>This is the about page</p>
+    </DefaultLayout>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import DefaultLayout from './../components/DefaultLayout';
+
+const Index = () => (
+    <DefaultLayout>
+        <p>Hello Next.js</p>
+    </DefaultLayout>
+);
+
+export default Index;
